8acaaa252ceb60e4fe9cd3da6a89f257837b6c04
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-12-03 14:44:59 +0700

    edited

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,16 +30,21 @@
                     </div>
                     @endif
                     {{ __('You are logged in!') }}
-
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-body">
                     <form class="w3-container w3-display-middle w3-card-4 " method="POST" id="payment-form"
-                        action="/payment/add-funds/paypal">
-                        {{ csrf_field() }}
+                        action="{{ route('paypal') }}">
+                        @csrf
                         <h2 class="w3-text-blue">Payment Form</h2>
                         <p>Demo PayPal form - Integrating paypal in laravel</p>
                         <p>
                             <label class="w3-text-blue"><b>Enter Amount</b></label>
-                            <input class="w3-input w3-border" name="amount" type="text"></p>
-                        <button class="w3-btn w3-blue">Pay with PayPal</button></p>
+                            <input class="w3-input w3-border" name="amount" type="text">
+                        </p>
+                        <button class="btn btn-primary">Pay with PayPal</button>
                     </form>
                 </div>
             </div>
